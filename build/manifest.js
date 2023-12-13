3953be6eeb3575fde5ca819f048245d9ef7623cc
Mark Kenny <beingmrkenny@gmail.com>
2023-12-13 13:22:36 +0000

    Manifest 3; ditching gulp; popup width bug in crom

---
+++ build/manifest.js
@@ -0,0 +1,35 @@
+// npm run manifest 2/3
+
+exports.compileManifest = (arguments) => {
+
+	const args = typeof arguments == 'undefined' ? process.argv : arguments;
+
+	if (!args.includes('2') && !args.includes('3')) {
+		console.log('Need to specify which version you want: 2 or 3');
+		return;
+	}
+
+	const manifestVersion = args.includes('2') ? 2 : 3;
+	const fs = require('fs');
+	const manifest = JSON.parse(fs.readFileSync('manifest.json', 'utf8'));
+
+	manifest.manifest_version = parseInt(manifestVersion);
+
+	const browserAction = manifest.browser_action || manifest.action;
+
+	delete manifest.browser_action;
+	delete manifest.action;
+
+	if (manifestVersion == 2) {
+		manifest.browser_action = browserAction;
+	} else {
+		manifest.action = browserAction;
+	}
+
+	fs.writeFileSync(
+		'Extension/manifest.json',
+		JSON.stringify(manifest, null, 2)
+	);
+};
+
+exports.compileManifest();
